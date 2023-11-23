2c266eaa06a83532cbf75a7c2dfb962582875916
Jawad Malik <jmalik94@outlook.com>
2023-11-23 21:19:35 +0000

    functions for basic operations defined

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num2 - num1;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num2 / num1;
+}
+
+// testing
+console.log(add(5, 6))
+console.log(subtract(5, 6))
+console.log(multiply(5, 6))
+console.log(divide(5, 6))
