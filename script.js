4bc98ee43779ddc920ce19e0694d77fbd3678f0d
Jawad Malik <jmalik94@outlook.com>
2023-12-03 21:01:39 +0000

    removed operate() function as unneccessary and calculate() function serves the intended purpose

--- script.js
+++ script.js
@@ -51,22 +51,6 @@ function divide(num1, num2) {
     display.appendChild(resultSpanElement);
 }
 
-function operate(num1, operator, num2) {
-    if (operator === "+") {
-        return add(num1, num2);
-    } else if (operator === "-") {
-        return subtract(num1, num2);
-    } else if (operator === "*") {
-        return multiply(num1, num2);
-    } else if (operator === "/") {
-        return divide(num1, num2);
-    } else {
-        console.log('calculaton is not valid. enter a valid calculation')
-    }
-}
-
-
-
 function calculate() {
     let calculationSpans = document.getElementsByClassName('calculation');
     let calculationArray = [];
